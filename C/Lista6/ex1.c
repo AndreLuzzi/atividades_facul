6d6bb9f147b5d59e59088d4fb126fefbd38df7a4
AndreLuzzi <andreluzzi@outlook.com.br>
2024-10-23 21:47:09 -0300

    lista6

---
+++ C/Lista6/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    int matriz[4][5];
+
+    for (int l=0;l<4;l++){
+        for (int c=0;c<4;c++){
+            printf("matriz[%d][%d]:",l,c);
+            scanf("%d", matriz [l][c]);
+        }
+        
+        
+    }
+    
+
+}
