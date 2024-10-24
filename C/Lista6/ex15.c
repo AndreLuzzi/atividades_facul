9d26a732e7d63e48e9d11358af804af618d9a257
AndreLuzzi <andreluzzi@outlook.com.br>
2024-10-23 22:27:25 -0300

    lista6

---
+++ C/Lista6/ex15.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main(){
+    int matriz[6][6];
+
+    for(int l=0;l<6;l++){
+        for(int c=0;c<6;c++){
+            printf("matriz[%d][%d]:",l,c);
+            scanf("%d",&matriz[l][c]);
+        }
+    }
+
+    int soma = 0;
+    for(int i=0;i<6;i++){
+        soma = soma + matriz[i][i];
+    }
+
+
+
+    for(int l=0;l<6;l++){
+        for(int c=0;c<6;c++){
+            printf("%d",matriz[l][c]);
+        }
+        printf("\n");
+    }
+
+    printf("\n %d",soma);
+
+}
