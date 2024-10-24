45396c7977c95dc769c4f5e48c5ed1ee07f985ca
AndreLuzzi <andreluzzi@outlook.com.br>
2024-10-23 22:09:19 -0300

    lista6

--- C/Lista6/matriz.c
+++ C/Lista6/matriz.c
@@ -4,13 +4,11 @@ void main(){
     int matriz[4][5];
 
     for (int l=0;l<4;l++){
-        for (int c=0;c<4;c++){
+        for (int c=0;c<5;c++){
             printf("matriz[%d][%d]:",l,c);
-            scanf("%d", matriz [l][c]);
+            scanf("%d",&matriz [l][c]);
         }
-        
-        
     }
-    
+
 
 }
