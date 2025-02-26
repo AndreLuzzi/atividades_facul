bd9027d267f0377bfbce9893a95d2e8ed631c4d7
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-02-26 20:28:19 -0300

    realizado em aula

---
+++ Banco de Dados/aula3.sql
@@ -0,0 +1,26 @@
+create table pizza(
+	id_pizza  serial primary key,
+	tamanho varchar(10),
+	preco real
+	constraint fk_ingredientes foreign key (id_ingrediente) references ingrediente(id_ingrediente)
+	constraint fk_sabores foreign key (id_sabor) references sabores(id_sabor)
+);
+
+create table ingredientes(
+	id_ingrediente  serial primary key,
+	nome varchar(30),
+	quantidade int,
+	descricao varchar(200)
+);
+
+create table sabores(
+	id_sabor  serial primary key,
+	nome varchar(30),
+	descricao varchar(200)
+);
+
+create table sabores_ingredientes(
+	id_sab_ing serial primary key,
+constraint fk_ingredientes foreign key (id_ingrediente) references ingrediente(id_ingrediente)
+constraint fk_sabores foreign key (id_sabor) references ingrediente(id_sabor)
+);
