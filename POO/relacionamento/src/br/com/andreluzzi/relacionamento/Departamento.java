07e6ceff0c01676ce078a8aba20b0a4d9c24670a
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-03-24 22:24:18 -0300

    realizado em sala

---
+++ POO/relacionamento/src/br/com/andreluzzi/relacionamento/Departamento.java
@@ -0,0 +1,29 @@
+package br.com.andreluzzi.relacionamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Departamento {
+
+    private String nome;
+    private List<Funcionario> funcionarios;
+
+    public Departamento(String nome) {
+        this.funcionarios = new ArrayList<>();
+        this.nome = nome;
+    }
+
+    public void adicionarFuncionario(Funcionario funcionario){
+        funcionarios.add(funcionario);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+}
+
---
+++ POO/relacionamento/src/br/com/andreluzzi/relacionamento/Funcionario.java
@@ -0,0 +1,15 @@
+package br.com.andreluzzi.relacionamento;
+
+public class Funcionario {
+
+    private String nome;
+
+    public Funcionario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
