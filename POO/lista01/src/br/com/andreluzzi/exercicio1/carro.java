330f7af5dd4438302ca4885dadb30f080570f4d4
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-04-03 19:55:10 -0300

    realizado em sala

--- POO/lista01/src/br/com/andreluzzi/exercicio1/carro.java
+++ POO/lista01/src/br/com/andreluzzi/exercicio1/carro.java
@@ -1,13 +1,13 @@
 package br.com.andreluzzi.exercicio1;
 
-public class carro {
+public class Carro {
     String marca;
     String modelo;
     int ano;
 
     void mostrarcarro() {
-        System.out.println("Marca: " + marca);
-        System.out.println("Modelo: " + modelo);
+        System.out.println("Marca do carro e: " + marca);
+        System.out.println("Seu modelo: " + modelo);
         System.out.println("Ano: " + ano);
     }
    
--- POO/lista01/src/br/com/andreluzzi/exercicio1/principal.java
+++ POO/lista01/src/br/com/andreluzzi/exercicio1/principal.java
@@ -1,6 +1,6 @@
 package br.com.andreluzzi.exercicio1;
 
-public class principal {
+public class Principal {
     public static void main(String[] args) {
         Carro meuCarro = new Carro();
         meuCarro.marca = "Toyota";
