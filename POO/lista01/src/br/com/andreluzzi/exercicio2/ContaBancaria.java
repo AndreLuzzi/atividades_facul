115f156a16165a575892b77caebe2e749d904dc0
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-04-03 20:20:52 -0300

    realizado em sala

--- POO/lista01/src/br/com/andreluzzi/exercicio2/ContaBancaria.java
+++ POO/lista01/src/br/com/andreluzzi/exercicio2/ContaBancaria.java
@@ -1,5 +1,23 @@
 package br.com.andreluzzi.exercicio2;
 
 public class ContaBancaria {
+    String titular;
+    float saldo;
 
+    void depositar(double valor){
+        saldo += valor;
+    }
+
+    void sacar(double valor){
+        if (saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
+    void exibirSaldo(){
+        System.out.println("Bem vindo: " + titular);
+        System.out.println("Seu daldo atual: R$ " + saldo);
+    }
 }
--- POO/lista01/src/br/com/andreluzzi/exercicio2/principal.java
+++ POO/lista01/src/br/com/andreluzzi/exercicio2/principal.java
@@ -2,4 +2,13 @@ package br.com.andreluzzi.exercicio2;
 
 public class principal {
 
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria();
+        conta.titular = "João Silva";
+        conta.saldo = 1000.00;
+        conta.depositar(500);
+        conta.sacar(300);
+        conta.exibirSaldo();
+    }
 }
+       
