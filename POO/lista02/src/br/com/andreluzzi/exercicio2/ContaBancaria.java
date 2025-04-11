117bd2c9663f52501c4b6929550ab4a8904833ae
AndreLuzzi <andreluzzi@outlook.com.br>
2025-04-11 15:48:06 -0300

    realizado em casa

--- POO/lista02/src/br/com/andreluzzi/exercicio2/ContaBancaria.java
+++ POO/lista02/src/br/com/andreluzzi/exercicio2/ContaBancaria.java
@@ -1,5 +1,33 @@
 package br.com.andreluzzi.exercicio2;
 
 public class ContaBancaria {
+    private String numerodaConta;
+    private String titular;
+    private double saldo;
 
+    public String getNumerodaConta() {
+        return numerodaConta;
+    }
+
+    public void setNumerodaConta(String numerodaConta) {
+        this.numerodaConta = numerodaConta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
 }
--- POO/lista02/src/br/com/andreluzzi/exercicio2/Principal.java
+++ POO/lista02/src/br/com/andreluzzi/exercicio2/Principal.java
@@ -1,5 +1,14 @@
 package br.com.andreluzzi.exercicio2;
 
 public class Principal {
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria();
+        conta.setNumerodaConta("7654321");
+        conta.setSaldo(1000);
+        conta.setTitular("Klebinho");
 
+        System.out.println("Titular da conta: " + conta.getTitular());
+        System.out.println("Numero da conta: " + conta.getNumerodaConta());
+        System.out.println("Saldo: " + conta.getSaldo());
+    }
 }
