d1880c4cdd7e448a22c64cce1ab99ec48ef5bf7c
AndreLuzzi <andreluzzi@outlook.com.br>
2025-04-11 15:31:42 -0300

    realizado em casa

--- POO/lista02/src/br/com/andreluzzi/exercicio1/Aluno.java
+++ POO/lista02/src/br/com/andreluzzi/exercicio1/Aluno.java
@@ -1,5 +1,20 @@
 package br.com.andreluzzi.exercicio1;
 
 public class Aluno {
+    String nome;
+    int idade;
+    String matricula;
 
+    public Aluno(String nome, int idade, String matricula) {
+        this.nome = nome;
+        this.idade = idade;
+        this.matricula = matricula;
+    }
+
+    void informacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Matricula: " + matricula);
+    }
+    
 }
--- POO/lista02/src/br/com/andreluzzi/exercicio1/Principal.java
+++ POO/lista02/src/br/com/andreluzzi/exercicio1/Principal.java
@@ -2,4 +2,9 @@ package br.com.andreluzzi.exercicio1;
 
 public class Principal {
 
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("Klebinho", 22, "7654321");
+        aluno.informacoes();
+        
+    }
 }
