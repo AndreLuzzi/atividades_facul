81c59688d568e55b6ede0a85cf474e8f0bdf77f4
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-04-28 21:38:17 -0300

    realizado em sala

---
+++ POO/heranca/src/br/com/andreluzzi/exercicio1/Moto.java
@@ -0,0 +1,5 @@
+package br.com.andreluzzi.exercicio1;
+
+public class Moto extends Veiculo {
+    String cilindrada;
+}
---
+++ POO/heranca/src/br/com/andreluzzi/exercicio1/Veiculo.java
@@ -0,0 +1,7 @@
+package br.com.andreluzzi.exercicio1;
+
+public class Veiculo {
+    String cor;
+    String marca;
+    String modelo;
+}
