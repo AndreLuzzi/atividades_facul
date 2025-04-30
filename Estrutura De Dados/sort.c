ad5ff3ea9ad63f464067d50a6cedab3c23bc24cf
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-04-29 22:02:59 -0300

    realizado em sala

---
+++ Estrutura De Dados/sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Função que implementa o Selection Sort
+void selectionSort(int v[200], int n) {
+    int i, j, aux, min;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (v[j] < v[min]) {
+                min = j;
+            }
+        }
+        // Troca os elementos
+        aux = v[i];
+        v[i] = v[min];
+        v[min] = aux;
+    }
+}
+
+// Função principal
+int main() {
+    int v[200], n, i;
+
+    printf("Entre o tamanho desejado do vetor: ");
+    scanf("%d", &n);
+
+    printf("Entre os %d elementos do vetor:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+    }
+
+    selectionSort(v, n);
+
+    printf("\nVetor ordenado:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\t", v[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
