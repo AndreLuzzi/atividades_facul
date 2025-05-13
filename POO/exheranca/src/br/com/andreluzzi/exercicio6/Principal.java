92e59d633fc5415849da3697bc32abae68867dfe
AndreLuzzi <andreluzzi@outlook.com.br>
2025-05-12 23:41:13 -0300

    realizado em casa

--- POO/exheranca/src/br/com/andreluzzi/exercicio6/Principal.java
+++ POO/exheranca/src/br/com/andreluzzi/exercicio6/Principal.java
@@ -1,8 +1,8 @@
 package br.com.andreluzzi.exercicio6;
 
-public public class Principal {
+public class Principal {
     public static void main(String[] args) {
-        Professor p = new Professor("João", 45, "Matemática");
+        Professor p = new Professor("Joao", 45, "Matematica");
         Aluno a = new Aluno("Maria", 20, "Engenharia");
 
         p.apresentar();
---
+++ POO/exheranca/src/br/com/andreluzzi/exercicio7/Principal.java
@@ -0,0 +1,11 @@
+package br.com.andreluzzi.exercicio7;
+
+public class Principal {
+    public static void main(String[] args) {
+        Smartphone s = new Smartphone("Samsung", "Galaxy S21", "Android");
+        Televisao t = new Televisao("LG", "OLED55", 55);
+
+        s.ligar();
+        t.ligar();
+    }
+}
