0c7d24a7ada18e931c93f2cd12c7d2dd14c03534
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-05-13 21:46:05 -0300

    realizado em casa

--- Banco De Dados/filme.sql
+++ Banco De Dados/filme.sql
@@ -81,3 +81,86 @@ INSERT INTO ator2 (nome) VALUES ('Leonardo da Vinti Reais');
 select *from filme_ator
 select *from filme
 select *from ator
+
+select nome from ator union select nome from ator2
+select nome from ator union all select nome from ator2
+select a.nome, c cache from ator a cross join ator_cache c
+
+--1	
+SELECT A.NOME, F.NOME FROM ATOR A 
+INNER JOIN FILME_ATOR FA
+ON A.IDATOR=FA.IDATOR
+INNER JOIN FILME F
+
+--2
+select a.nome, c cache from ator a cross join ator_cache c
+
+
+--3
+SELECT A.NOME, F.NOME FROM ATOR A 
+RIGHT JOIN FILME_ATOR FA
+ON A.IDATOR=FA.IDATOR
+INNER JOIN FILME F
+ON FA.IDFILME=F.IDFILME
+
+--4
+create table diretor(
+	id_diretor serial primary key,
+	nome varchar(20),
+	data_nascimento date,
+)
+create table diretor_filme(
+	id serial primary key,
+	id_filme int,
+	id_diretor int,
+	constraint fk_diretor foreign key (id_diretor) from diretor(id_diretor)
+	constraint fk_filme foreign key (idfilme) from filme(idfilme)
+)
+	
+insert into diretor (nome,data_nascimento) values ('Andre', 2003-02-15),('Leonardo', 2004-09-14),('Henrique', 2001-10-31);
+insert into diretor_filme (id_diretor,id_filme) values (1,1),(1,2),(2,3),(2,4),(3,5),(3,6);
+
+--5
+create table genero(
+	id_genero serial primary key,
+	nome varchar(20)
+)
+create table genero_filme(
+	id_genero_filme serial primary key,
+	id_genero int,
+	idfilme int,
+	constraint fk_genero foreign key (id_genero) references genero(id_genero),
+	constraint fk_filme foreign key (idfilme) references filme(idfilme)
+)
+insert into genero (nome) values ('Comédia'),('Aventura'),('Ação'),('Drama'),('Terror'),('Ficção cientifica'),('Romance'),('Animação');
+
+--6
+select f.nome as nome_filme, 
+       d.nome as nome_diretor, 
+       g.nome as genero
+from filme f
+inner join diretor_filme df on f.idfilme = df.idfilme
+inner join diretor d on df.id_diretor = d.id_diretor
+inner join genero_filme fg on f.idfilme = fg.idfilme
+inner join genero g on fg.id_genero = g.id_genero
+order by f.nome, d.nome, g.nome;
+
+--7
+create table diretor_cache (
+  id serial primary key,
+  id_diretor int,
+  cache int,
+  constraint fk_diretor foreign key (id_diretor) references diretor(id)
+)
+
+insert into diretor_cache (id_diretor, cache) values (1, 10000), (2, 8000), (3, 9000);
+
+--8
+select f.nome as nome_filme, a.nome as nome_ator from filme f cross join ator a;
+
+--9
+select a.nome, c.cache from ator a left join ator_cache c on a.idator = c.idator;
+
+--10
+select nome from ator union select nome from ator2;
+select nome from ator union all select nome from ator2;
