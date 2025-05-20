ce64f53b7f2cae237e70230b38dc096d2f2b6d51
Andre Cappellesso Luzzi <andreluzzi@outlook.com.br>
2025-05-19 22:21:32 -0300

    realizado em casa

---
+++ POO/interface/src/br/com/andreluzzi/exemplo1/Animal.java
@@ -0,0 +1,21 @@
+package br.com.andreluzzi.interface.exemplo1;
+
+public class Animal implements SerVivo{
+     public Animal() {
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("movendo");
+    }
+
+    @Override
+    public void comer(int massa) {
+        System.out.println("comendo");
+    }
+
+    @Override
+    public void info() {
+        System.out.println("info");
+    }
+}
