ea5de2341bc9e0413262e735016905562aa39d2e
AndreLuzzi <andreluzzi@outlook.com.br>
2025-07-02 16:56:26 -0300

    realizado em casa

---
+++ POO/intrerface e generics/src/br/com/andreluzzi/teste1/Animal.java
@@ -0,0 +1,6 @@
+package br.com.andreluzzi.teste1;
+
+public class Animal {
+    public string nome;
+    public string anda;
+}
---
+++ POO/intrerface e generics/src/br/com/andreluzzi/teste1/gato.java
@@ -0,0 +1,5 @@
+package br.com.andreluzzi.teste1;
+
+public class Gato {
+    
+}
